06112ed98c0ce8e77ef041c0f45b25d9abf1489f
Uriah Welcome <precision@devrandom.net>
2016-04-20 21:10:49 -0700

    adding Copyright headers, minor formatting cleanup

--- experimental/arduino101/science-journal-arduino/science-journal-arduino.ino
+++ experimental/arduino101/science-journal-arduino/science-journal-arduino.ino
@@ -1,104 +1,113 @@
+/*
+ *  Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 #include <CurieBLE.h>
 #include "goosci_utility.h"
 #include "arduino_nrf_pins.h"
 #include "heartbeat.h"
+
 #define SENSOR_PIN A0    // Input Pin to read from
 
 BLEPeripheral blePeripheral; // create peripheral instance
 BLEService whistlepunkService("555a0001-0aaa-467a-9538-01f0652c74e8"); // create service
 // Must be 20 char long to accomodate full-size messages.
 const char *value = "                     ";
-
-String BleLongName = "Sci";
-bool serialConnected = false;
-
 BLECharacteristic valueCharacteristic("555a0003-0aaa-467a-9538-01f0652c74e8", BLENotify, value);
 
 #include "internal/ble_client.h"
 #include "services/ble/ble_service_gap_api.h"
-   
+
+String BleLongName = "Sci";
+bool serialConnected = false;
 
 void blePeripheralConnectHandler(BLECentral& central) {
-  // central connected event handler
-  DEBUG_PRINT("Connected event, central: ");
-  DEBUG_PRINTLN(central.address());
+    // central connected event handler
+    DEBUG_PRINT("Connected event, central: ");
+    DEBUG_PRINTLN(central.address());
 }
 
 void blePeripheralDisconnectHandler(BLECentral& central) {
-  // central disconnected event handler
-  DEBUG_PRINT("Disconnected event, central: ");
-  DEBUG_PRINTLN(central.address());
+    // central disconnected event handler
+    DEBUG_PRINT("Disconnected event, central: ");
+    DEBUG_PRINTLN(central.address());
 }
 
-
 void bleNotificationSubscribeHandler(BLECentral& central, BLECharacteristic& characteristic) {
-  // value characteristic event handler
-  DEBUG_PRINT("Subscribe event, central: ");
-  DEBUG_PRINTLN(central.address());
+    // value characteristic event handler
+    DEBUG_PRINT("Subscribe event, central: ");
+    DEBUG_PRINTLN(central.address());
 }
 
 void bleNotificationUnsubscribeHandler(BLECentral& central, BLECharacteristic& characteristic) {
-  // value characteristic unsubscribe event handler
-  DEBUG_PRINT("Unsubscribe event, central: ");
-  DEBUG_PRINTLN(central.address());
+    // value characteristic unsubscribe event handler
+    DEBUG_PRINT("Unsubscribe event, central: ");
+    DEBUG_PRINTLN(central.address());
 }
 
 void setup() {
-  wait_for_serial();
-  
-  // set the local name peripheral advertises
-  blePeripheral.setLocalName("Initial");
-  // set the UUID for the service this peripheral advertises:
-  blePeripheral.setAdvertisedServiceUuid(whistlepunkService.uuid());
+    wait_for_serial();
 
-  // add service and characteristics
-  blePeripheral.addAttribute(whistlepunkService);
-  blePeripheral.addAttribute(valueCharacteristic);
+    // set the local name peripheral advertises
+    blePeripheral.setLocalName("Initial");
+    // set the UUID for the service this peripheral advertises:
+    blePeripheral.setAdvertisedServiceUuid(whistlepunkService.uuid());
 
+    // add service and characteristics
+    blePeripheral.addAttribute(whistlepunkService);
+    blePeripheral.addAttribute(valueCharacteristic);
 
-  // assign event handlers for connected, disconnected to peripheral
-  blePeripheral.setEventHandler(BLEConnected, blePeripheralConnectHandler);
-  blePeripheral.setEventHandler(BLEDisconnected, blePeripheralDisconnectHandler);
-  valueCharacteristic.setEventHandler(BLESubscribed, bleNotificationSubscribeHandler);
-  valueCharacteristic.setEventHandler(BLEUnsubscribed, bleNotificationUnsubscribeHandler);
-  
-  ble_addr_t _local_bda;
-  char       _device_name[BLE_MAX_DEVICE_NAME+1];  
-  ble_client_get_factory_config(&_local_bda, _device_name);
-  BleLongName += String(_local_bda.addr[4], HEX);
-  BleLongName += String(_local_bda.addr[5], HEX);
-  DEBUG_PRINT("Address is: ");
-  DEBUG_PRINTLN(BleLongName);
-  blePeripheral.setLocalName(BleLongName.c_str());
+    // assign event handlers for connected, disconnected to peripheral
+    blePeripheral.setEventHandler(BLEConnected, blePeripheralConnectHandler);
+    blePeripheral.setEventHandler(BLEDisconnected, blePeripheralDisconnectHandler);
+    valueCharacteristic.setEventHandler(BLESubscribed, bleNotificationSubscribeHandler);
+    valueCharacteristic.setEventHandler(BLEUnsubscribed, bleNotificationUnsubscribeHandler);
 
-  // advertise the service
-  blePeripheral.begin();  
+    ble_addr_t _local_bda;
+    char       _device_name[BLE_MAX_DEVICE_NAME+1];
+    ble_client_get_factory_config(&_local_bda, _device_name);
+    BleLongName += String(_local_bda.addr[4], HEX);
+    BleLongName += String(_local_bda.addr[5], HEX);
+    DEBUG_PRINT("Address is: ");
+    DEBUG_PRINTLN(BleLongName);
+    blePeripheral.setLocalName(BleLongName.c_str());
+
+    // advertise the service
+    blePeripheral.begin();
 }
 
 
 void loop() {
-  if (Serial) {
-    if (!serialConnected) {
-        serialConnected = true;
-        DEBUG_PRINT(F("LongName: "));
-        DEBUG_PRINTLN(BleLongName);
+    if (Serial) {
+        if (!serialConnected) {
+            serialConnected = true;
+            DEBUG_PRINT(F("LongName: "));
+            DEBUG_PRINTLN(BleLongName);
+        }
+    } else {
+        if (serialConnected)
+            serialConnected = false;
     }
-  } else {
-    if (serialConnected)
-        serialConnected = false;
-  }
 
-  // poll peripheral
-  blePeripheral.poll();
+    // poll peripheral
+    blePeripheral.poll();
 
-  if (valueCharacteristic.subscribed()) {
-      int sensorValue = analogRead(SENSOR_PIN);
-      send_data(valueCharacteristic, millis(), sensorValue);
-  }
+    if (valueCharacteristic.subscribed()) {
+        int sensorValue = analogRead(SENSOR_PIN);
+        send_data(valueCharacteristic, millis(), sensorValue);
+    }
 #ifdef GOOSCI_DEVELOPER_MODE
-  heartbeat();
+    heartbeat();
 #endif
-
-  heartbeat();
 }
-
